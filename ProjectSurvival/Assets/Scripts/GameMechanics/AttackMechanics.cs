ab345ab5ff5e39e95fe342f1801e1d97f969ba00
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-01-21 17:24:35 -0500

    Basic attack animation added to player

---
+++ ProjectSurvival/Assets/Scripts/GameMechanics/AttackMechanics.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackMechanics : MonoBehaviour {
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        attack(Input.GetButtonDown("Fire1"));
+    }
+
+    void attack(bool attackDown)
+    {
+        if (attackDown)
+        {
+            anim.ResetTrigger("Attack");
+            anim.SetTrigger("Attack");
+
+        }
+    }
+}
