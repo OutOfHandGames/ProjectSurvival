b9bdd55180c3e0dc3bd5de447fe137939da59e99
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-01-23 15:34:52 -0500

    HealthMechanics

---
+++ ProjectSurvival/Assets/Scripts/BasicGameMechanics/HealthMechanics.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthMechanics : MonoBehaviour {
+    public float maxHealth = 100;
+
+    bool isDead;
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    protected virtual void deathLogic()
+    {
+        isDead = true;
+    }
+
+    public void decreaseHealth(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0 && !isDead)
+        { 
+            deathLogic();
+        }
+    }
+
+    public void increaseHealth(float health)
+    {
+        currentHealth += health;
+    }
+
+    public void setHealth(float health)
+    {
+        this.currentHealth = health;
+        if (currentHealth <= 0 && !isDead)
+        {
+            deathLogic();
+        }
+    }
+}
