06c9f71319ca8bbe08dcabed416221d1c146a93a
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-01-25 02:24:57 -0500

    Some AI StateMachine logic

---
+++ ProjectSurvival/Assets/Scripts/AIScripts/AIStateMachine.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIStateMachine : MonoBehaviour {
+    public State[] currentStates = new State[0];
+
+
+    void Update()
+    {
+        foreach(State s in currentStates)
+        {
+            if (s != null)
+            {
+                s.updateState(Time.deltaTime);
+            }
+        }
+    }
+
+    public void changeState(State newState)
+    {
+        currentStates[0].endState();
+        currentStates[0] = newState;
+        currentStates[0].startState();
+    }
+
+
+}
